b5feddb5fa0100506149d2627f5d129a6f4ad15f
Krystof Rezac <krystofrezac@gmail.com>
2021-10-02 10:53:53 +0200

    Add udiskie

--- scripts/install_base_packages.sh
+++ scripts/install_base_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo pacman -S git xclip htop tldr
-yay -S google-chrome asdf-vm
+sudo pacman -S git xclip htop tldr udiskie
+day -S google-chrome asdf-vm
 
 cp -f /opt/asdf-vm ~/.asdf
 
