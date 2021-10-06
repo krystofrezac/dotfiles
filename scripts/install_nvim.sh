898ea2e389f282882eaad4ce2e6bf220b8841417
Kryštof Řezáč <krystofrezac@gmail.com>
2021-10-06 14:05:04 +0200

    Fix fira code in kitty

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -4,6 +4,7 @@ asdf plugin add neovim
 asdf install neovim latest
 asdf global neovim latest
 
+sudo pacman -S ccls
 rm -rf "$HOME/.config/nvim"
 ln -s "$DOTFILES/configs/nvim" "$HOME/.config/nvim"
 
