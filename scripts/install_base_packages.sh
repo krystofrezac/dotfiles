c0a998dc7253922438007b57c3260a0562f89a68
Kryštof Řezáč <krystofrezac@gmail.com>
2021-10-06 10:14:38 +0200

    Edit installing git

--- scripts/install_base_packages.sh
+++ scripts/install_base_packages.sh
@@ -7,6 +7,8 @@ yay -S google-chrome asdf-vm
 cp -f /opt/asdf-vm ~/.asdf
 
 # git
+git config --global user.email "krystofrezac@gmail.com"
+git config --global user.name "Kryštof Řezáč"
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 
 echo Installation complete
