f9b63416dea627e08ce7ebb9fb550ad8ce16b022
Kryštof Řezáč <krystofrezac@gmail.com>
2021-10-06 14:20:24 +0200

    Fix feh

--- configs/X11/notebook.sh
+++ configs/X11/notebook.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+NOTEBOOK_SCREEN="eDP-1"
 xrandr --newmode "1500x1000_60.00"  124.25  1500 1600 1752 2000  1000 1003 1013 1038 -hsync +vsync
-xrandr --addmode Virtual-1 1500x1000_60.00
-xrandr --output Virtual-1 --primary --mode 1500x1000_60.00 --pos 0x0 --rotate normal --output Virtual-2 --off --output Virtual-3 --off --output Virtual-4 --off
+xrandr --addmode "$NOTEBOOK_SCREEN" 1500x1000_60.00
+xrandr --output "$NOTEBOOK_SCREEN" --primary --mode 1500x1000_60.00 --pos 0x0 --rotate normal
