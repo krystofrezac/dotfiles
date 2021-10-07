7e571ba9d082dc8c43de9d01d81f6bcfc5c118d8
Kryštof Řezáč <krystofrezac@gmail.com>
2021-10-07 15:39:03 +0200

    Fix monitor setup

--- configs/X11/configs/home.sh
+++ configs/X11/configs/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 3000x2000 --scale 0.5x0.5 --pos 0x0 --rotate normal --output DP-1 --primary --mode 1920x1080 --pos 3000x0 --rotate normal --output DP-2 --off --output HDMI-1 --off
+xrandr --output eDP-1 --mode 3000x2000 --scale 0.5x0.5 --pos 0x80 --rotate normal --output DP-1 --primary --mode 1920x1080 --pos 1500x0 --rotate normal --output DP-2 --off --output HDMI-1 --off
