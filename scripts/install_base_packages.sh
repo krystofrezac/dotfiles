5bf1fdb9adb8e6cdec653eeb95efd46330a1a53a
Kryštof Řezáč <krystofrezac@gmail.com>
2021-10-08 14:47:20 +0200

    Add brightnes control

--- scripts/install_base_packages.sh
+++ scripts/install_base_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S git diff-so-fancy xclip htop tldr udiskie firefox
+sudo pacman -S git diff-so-fancy xclip htop tldr udiskie firefox brightnessctl
 yay -S google-chrome asdf-vm
 # asdf
 cp -f /opt/asdf-vm ~/.asdf
@@ -9,5 +9,7 @@ cp -f /opt/asdf-vm ~/.asdf
 git config --global user.email "krystofrezac@gmail.com"
 git config --global user.name "Kryštof Řezáč"
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+
+git config --global alias.ll "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
 echo Installation complete
