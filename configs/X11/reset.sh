40bc0bdf7ffdae1f8ec26b02960452a8bb06dd72
Kryštof Řezáč <krystofrezac@gmail.com>
2021-10-12 12:40:08 +0200

    Fix drivers

--- configs/X11/reset.sh
+++ configs/X11/reset.sh
@@ -1,1 +1,1 @@
-xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output HDMI-1 --off
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output HDMI1 --off
--- scripts/install_gui.sh
+++ scripts/install_gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S xorg xorg-xinit kitty i3-wm i3status dmenu i3lock imagemagick scrot arandr feh ttf-fira-code dunst libnotify nvidia network-manager-applet nautilus noto-fonts-emoji 
+sudo pacman -S xorg xorg-xinit kitty i3-wm i3status dmenu i3lock imagemagick scrot arandr feh ttf-fira-code dunst libnotify nvidia nvidia-utils xf86-video-intel network-manager-applet nautilus noto-fonts-emoji 
 yay -S nerd-fonts-fira-code 
 
 ln -sf "$DOTFILES/configs/xorg-xinit/.xinitrc" "$HOME/.xinitrc"
