36c51be348adaf1b1cd088a873b2bcd03d408c09
Kryštof Řezáč <krystofrezac@gmail.com>
2021-11-20 19:28:39 +0100

    Add git diffFilter

--- scripts/install_base_packages.sh
+++ scripts/install_base_packages.sh
@@ -9,6 +9,7 @@ cp -f /opt/asdf-vm ~/.asdf
 git config --global user.email "krystofrezac@gmail.com"
 git config --global user.name "Kryštof Řezáč"
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
 
 git config --global alias.ll "log --graph --abbrev-commit --decorate --date=format:'%d.%m.%Y %H:%M' --format=format:'%C(bold blue)%h%C(reset) - %C(white)%s%C(reset) %C(dim white)- %an - %ad%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
